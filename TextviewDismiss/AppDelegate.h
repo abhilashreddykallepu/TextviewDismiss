0b7577a4c804c9f3da1f2c64645c43b47345cd3e
abhilashreddy <abhii.reddy6@gmail.com>
2013-12-26 10:32:26 +0530

    Initial Commit

---
+++ TextviewDismiss/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextviewDismiss
+//
+//  Created by Hadi Hatinoglu on 26/12/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextviewDismiss/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TextviewDismiss
+//
+//  Created by Hadi Hatinoglu on 26/12/13.
+//  Copyright (c) 2013 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
